46bad792a32611682f894a9639b2585b0d2181d1
magvan temuulen <magvan.temuulen@gmail.com>
2023-05-02 22:21:23 +0200

    begin home page

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
+import Typed from "typed.js"
+
+
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
+    new Typed(this.element, {
+      strings: ["TECHNOLOGIE FOR DESIGNER", "BY LUXE INTEGRATION"],
+      typeSpeed: 50,
+      loop: true
+    })
   }
 }
