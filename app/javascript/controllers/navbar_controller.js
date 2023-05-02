98cdca7abba66a26beddf540cca31c98bf31e0bd
magvan temuulen <magvan.temuulen@gmail.com>
2023-05-02 21:29:28 +0200

    add js packages

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add("navbar-lewagon-white")
+    } else {
+      this.element.classList.remove("navbar-lewagon-white")
+    }
+  }
+}
